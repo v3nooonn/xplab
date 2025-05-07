5f81b6669ccf93274a016559707c47af759b3516
p3ngf3i <v3nooom@outlook.com>
2025-05-07 11:03:58 +0800

    entrance

---
+++ xplab.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A simple hello-world Python project.
+"""
+
+def main():
+    """
+    Main function that prints a hello world message.
+    """
+    print("Hello, World!")
+
+"""
+To run this script:
+
+1. From the command line:
+   $ python xplab.py
+   
+2. Or make it executable (on Unix-like systems):
+   $ chmod +x xplab.py
+   $ ./xplab.py
+
+The script will print "Hello, World!" to the console.
+"""
+if __name__ == "__main__":
+    main()
